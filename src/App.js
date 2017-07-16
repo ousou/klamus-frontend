c64aaf439e1394a3ce348fb90b464d6b2d69d622
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-07-16 15:19:29 +0300

    Project structure for Klamus frontend

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="header">
+          <h2>Klassisen musiikin levytietokanta</h2>
+        </div>
+        <div className="content">
+        <p className="content-text">
+          Testiteksti
+        </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
