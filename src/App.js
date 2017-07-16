24a37a3a4b0ec6a975a1f51a273a2ef24fd81d73
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-07-16 21:30:34 +0300

    Started making save form, but it still doesn't work as expected

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      data: [],
+      formData: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -25,28 +28,66 @@ class App extends Component {
           });
         })
   }
+
+  handleFormSubmit(event) {
+    alert('Data was submitted: ' + JSON.stringify(this.state.formData));
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    var id = event.target.id;
+    var value = event.target.value;
+    switch (id) {
+      case 'name':
+      this.setState({
+        formData: {
+          composer: value
+        }
+      });
+        break;
+      case 'composer':
+        this.setState({
+          formData: {
+            composer: value
+          }
+        });
+        break;
+    }
+
+  }
+
   render() {
     return (
       <div className="main-container">
         <header className="header">
           <h2>Klassisen musiikin levytietokanta</h2>
         </header>
-      <Tabs className="tab-container" defaultActiveKey={1}>
+        <Tabs className="tab-container" defaultActiveKey={1}>
 
-        <Tab eventKey={1} title="Säveltäjät">
-          <div>
-            <h3>Säveltäjät</h3>
-            <ReactTable className="content-table"
-              data={this.state.data}
-              columns={composerColumns}
-            />
-          </div>
-        </Tab>
-        <Tab eventKey={2} title="Syötä uusi levy">
-          <h3>Syötä uusi levy</h3>
-          <span>Tähän stuffia</span>
-        </Tab>
-      </Tabs>
+          <Tab eventKey={1} title="Säveltäjät">
+            <div>
+              <h3>Säveltäjät</h3>
+              <ReactTable className="content-table"
+                data={this.state.data}
+                columns={composerColumns}
+              />
+            </div>
+          </Tab>
+          <Tab eventKey={2} title="Syötä uusi levy">
+            <h3>Syötä uusi levy</h3>
+            <form onSubmit={this.handleFormSubmit}>
+               <label>
+                 Levyn nimi:
+                 <input id="name" type="text" value={this.state.value} onChange={this.handleChange}/>
+               </label>
+               <label>
+                 Säveltäjä:
+                 <input id="composer" type="text" value={this.state.value} onChange={this.handleChange}/>
+               </label>
+               <input type="submit" value="Submit" />
+             </form>
+          </Tab>
+        </Tabs>
       </div>
 
     );
